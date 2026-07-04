87de7135c817d2973176e925e5f04579786b3fb2
shivanshu techie <shivanshup938@gmail.com>
2026-07-04 21:09:47 +0530

    Made ToDo List and learned Arrays and Loops

---
+++ java_script/11a-todo-list.js
@@ -0,0 +1,68 @@
+//-----------------Input 1------------------------------
+
+let todoList1 = JSON.parse(localStorage.getItem('todo-list1')) || [];
+
+function itemAdd1(){
+  const inputValue = document.querySelector('.input1').value;
+
+  todoList1.push(inputValue);
+
+  localStorage.setItem('todo-list1',JSON.stringify(todoList1));
+
+  // console.log(todoList1);
+
+  document.querySelector('.input1').value = '';
+}
+
+function eraseList(){
+  localStorage.removeItem('todo-list1');
+  todoList1 = [];
+  // console.log(todoList1);
+}
+
+//----------------Input 2-------------------------------
+
+let todoList2 = JSON.parse(localStorage.getItem('todo-list2')) ||
+[];
+
+function itemAdd2 (){
+  const  inputValue = document.querySelector('.input2').value;
+  const inputDate = document.querySelector('.dateInput').value;
+  const obj = {
+    name : inputValue,
+    date : inputDate
+  };
+  todoList2.push(obj);
+
+  // add in the storage
+  localStorage.setItem('todo-list2',JSON.stringify(todoList2));
+
+  // console.log(todoList2);
+
+  document.querySelector('.input2').value = '';
+
+  renderHistory();
+}
+
+function renderHistory() {
+  let show = '';
+  for(let i = 0;i < todoList2.length;i++){
+
+    show += `
+    <div class="js-date">${todoList2[i].date}</div>  
+    <div class="js-todo">${todoList2[i].name}</div>
+    <button class ="removeButton" onclick="
+      todoList2.splice(${i},1);
+      renderHistory();
+    ">Remove</button>
+    `;
+  }
+  document.querySelector('.show').innerHTML = show;
+}
+
+function eraseList2(){
+  localStorage.removeItem('todo-list2');
+  todoList2 = [];
+  // console.log(todoList2);
+}
+
